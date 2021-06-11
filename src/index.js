50188b0e422f961c77dfaa801fd4f91713d6028b
kittymercury <eissturm.rrr@gmail.com>
2021-06-11 16:05:01 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import 'babel-polyfill';
+import './styles.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/app';
+
+ReactDOM.render(<App/>, document.getElementById('app'));
