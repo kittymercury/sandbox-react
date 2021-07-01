e81012fc891457cad6e164d228e22f99d1539bac
kittymercury <eissturm.rrr@gmail.com>
2021-07-01 20:44:20 +0300

    added logics

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,68 +5,70 @@ export default class App extends React.Component {
     super(props);
 
     this.state = {
+      currentUser: 7,
       currentPage: 'Chats',
-      currentUser: 4,
-      currentChat: null,
+      currentChat: '',
       chatInput: '',
 
       users: [
-        { id: 1, name: 'Bob', status: 'online' },
-        { id: 2, name: 'Tom', status: 'offline' },
-        { id: 3, name: 'Jef', status: 'offline' },
-        { id: 4, name: 'Kok', status: 'online' },
-        { id: 5, name: 'Kek', status: 'online' },
+        { id: 1, name: 'Cut Corners', status: 'online' },
+        { id: 2, name: 'Jesse Pinkman', status: 'online' },
+        { id: 3, name: 'Walter White', status: 'offline' },
+        { id: 4, name: 'Herr Honka', status: 'online' },
+        { id: 5, name: 'Sasuke Uchiha', status: 'offline' },
+        { id: 6, name: 'Olga Tkachuk', status: 'offline' },
+        { id: 7, name: 'Freddie Mercury', status: 'online' },
       ],
 
       chats: [
-        { id: 1, participants: [4, 5] },
-        { id: 2, participants: [4, 2] },
-        { id: 3, participants: [4, 1] },
+        { id: 1, participants: [ 7, 1 ] },
+        { id: 2, participants: [ 7, 6 ] },
+        { id: 3, participants: [ 7, 3 ] },
       ],
 
       messages: [
-        { id: 1, chatId: 1, userId: 4, time: '15:51', content: 'Hey!' },
-        { id: 2, chatId: 1, userId: 5, time: '16:14', content: 'Hello!' },
-        { id: 3, chatId: 2, userId: 4, time: '14:11', content: 'What?' },
-        { id: 4, chatId: 2, userId: 2, time: '17:15', content: 'Say my name!' },
-        { id: 5, chatId: 3, userId: 4, time: '11:00', content: '.i.' },
-        { id: 6, chatId: 3, userId: 1, time: '13:01', content: '???' },
-      ],
+        { id: 1, userId: 1, chatId: 1, time: '00:00', content: 'I love you <3' },
+        { id: 2, userId: 7, chatId: 1, time: '00:01', content: 'I love you too <3' },
+        { id: 3, userId: 6, chatId: 2, time: '07:40', content: 'Віта, в тебе є черешні?' },
+        { id: 4, userId: 7, chatId: 2, time: '12:10', content: 'нажаль уже немає' },
+        { id: 5, userId: 3, chatId: 3, time: '11:00', content: 'I know that you are the one who knocks and always make' },
+        { id: 6, userId: 7, chatId: 3, time: '13:04', content: 'You are goddamn right' },
+      ]
     };
   }
 
-  handleFooterButtonClick = (page) => {
-    this.setState({ currentPage: page });
+  handleClickButtonFooter = (page) => {
+    this.setState({ currentPage: page })
   }
 
-  handleChatClick = (chat) => {
-    this.setState({ currentPage: 'Messages', currentChat: chat });
+  handleClickChat = (chat) => {
+    this.setState({ currentChat: chat, currentPage: 'Messages' })
   }
 
   handleChangeChatInput = (e) => {
-    this.setState({ chatInput: e.target.value });
+    this.setState({ chatInput: e.target.value })
   }
 
-  handleClickSendMessage = (e) => {
+  handleClickSendButton = (e) => {
     const currentUser = this.state.currentUser;
-    const currentChat = this.state.currentChat;
     const chatInput = this.state.chatInput;
+    const currentChat = this.state.currentChat;
+    const messages = this.state.messages;
 
     const newMessage = {
       id: +new Date(),
-      chatId: currentChat.id,
       userId: currentUser,
-      time: new Date().toTimeString().replace(/.*(\d{2}:\d{2}).*/, "$1"),
-      content: chatInput,
-    };
-    const newMessages = this.state.messages.concat(newMessage)
+      chatId: currentChat.id,
+      time: new Date().toLocaleTimeString(),
+      content: chatInput
+    }
 
-    this.setState({ chatInput: '', messages: newMessages });
+    this.setState({ chatInput: '', messages: messages.concat(newMessage) })
   }
 
   render() {
-    const currentPage = this.state.currentPage;
     const currentUser = this.state.currentUser;
+    const currentPage = this.state.currentPage;
     const currentChat = this.state.currentChat;
     const chatInput = this.state.chatInput;
 
@@ -79,42 +81,75 @@ export default class App extends React.Component {
         <div className="header">
           <div className="pointer" style={{ width: '100px' }}>{(currentPage === 'Chats') ? 'Edit' : ''}</div>
           <div>{currentPage}</div>
-          <div className="pointer" style={{ width: '100px' }}>{(currentPage === 'Chats') ? 'Create chat' : ''}</div>
+          <div className="pointer" style={{ width: '100px' }}>{(currentPage === 'Chats') ? 'Create new chat' : ''}</div>
         </div>
 
         {(currentPage === 'Contacts') && (
           <div className="content contacts">
-
+            <ul>
+              {users.map((user) => {
+                return (
+                  <li>
+                    <span>{user.name}</span>
+                    <span className={user.status}>{user.status}</span>
+                  </li>
+                )
+              })}
+            </ul>
           </div>
         )}
 
         {(currentPage === 'Chats') && (
           <div className="content chats">
+            <ul>
+              {chats.map((chat) => {
+                const participant = users.find((user) => user.id === chat.participants[1]);
 
+                return (
+                  <li onClick={() => this.handleClickChat(chat)}>
+                    {participant.name}
+                  </li>
+                )
+              })}
+            </ul>
           </div>
         )}
 
         {(currentPage === 'Settings') && (
           <div className="content settings">
-
+            <span>{users.find((user) => user.id === currentUser).name}</span>
+            <span className="you"> (you)</span>
           </div>
         )}
 
         {(currentPage === 'Messages') && (
           <div className="content messages">
-            
+            <ul>
+              {messages.filter((message) => message.chatId === currentChat.id).map((message) => {
+                const user = users.find((user) => user.id === message.userId);
+                const isCurrentUsersMessage = message.userId === currentUser;
+                const style = { textAlign: isCurrentUsersMessage ? 'right' : 'left' };
+
+                return (
+                  <li>
+                    <div style={style}>{user.name} {message.time}</div>
+                    <div style={style}>{message.content}</div>
+                  </li>
+                )
+              })}
+            </ul>
 
             <div style={{ display: 'flex' }}>
-              <input style={{ flex: 1 }} type="text" value={chatInput} onChange={this.handleChangeChatInput} />
-              <button onClick={this.handleClickSendMessage}>Send</button>
+              <input style={{ flex: 1 }} type="text" placeholder="Type your message here" value={chatInput} onChange={this.handleChangeChatInput}/>
+              <button onClick={this.handleClickSendButton}>Send</button>
             </div>
           </div>
         )}
 
         <div className="footer">
-          <button onClick={() => this.handleFooterButtonClick('Contacts')}>Contacts</button>
-          <button onClick={() => this.handleFooterButtonClick('Chats')}>Chats</button>
-          <button onClick={() => this.handleFooterButtonClick('Settings')}>Settings</button>
+          <button onClick={() => this.handleClickButtonFooter('Contacts')}>Contacts</button>
+          <button onClick={() => this.handleClickButtonFooter('Chats')}>Chats</button>
+          <button onClick={() => this.handleClickButtonFooter('Settings')}>Settings</button>
         </div>
       </div>
     );
