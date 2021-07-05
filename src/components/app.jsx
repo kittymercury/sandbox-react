38abe51df6e6bd93fc85851717ca410e7f69df5b
kittymercury <eissturm.rrr@gmail.com>
2021-07-05 20:37:43 +0300

    added logics

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -9,15 +9,16 @@ export default class App extends React.Component {
       currentPage: 'Chats',
       currentChat: '',
       chatInput: '',
+      theme: 'light',
 
       users: [
-        { id: 1, name: 'Cut Corners', status: 'online' },
-        { id: 2, name: 'Jesse Pinkman', status: 'online' },
-        { id: 3, name: 'Walter White', status: 'offline' },
-        { id: 4, name: 'Herr Honka', status: 'online' },
-        { id: 5, name: 'Sasuke Uchiha', status: 'offline' },
-        { id: 6, name: 'Olga Tkachuk', status: 'offline' },
-        { id: 7, name: 'Freddie Mercury', status: 'online' },
+        { id: 1, name: 'Cut Corners', status: 'online', avatar: './tg-imgs/corners.jpeg' },
+        { id: 2, name: 'Jesse Pinkman', status: 'online', avatar: './tg-imgs/jesse.jpg' },
+        { id: 3, name: 'Walter White', status: 'offline', avatar: './tg-imgs/walter.jpeg' },
+        { id: 4, name: 'Herr Honka', status: 'online', avatar: './tg-imgs/honka.jpg' },
+        { id: 5, name: 'Sasuke Uchiha', status: 'offline', avatar: './tg-imgs/unknown-woman.png' },
+        { id: 6, name: 'Olga Tkachuk', status: 'offline', avatar: './tg-imgs/unknown-woman.png' },
+        { id: 7, name: 'Freddie Mercury', status: 'online', avatar: './tg-imgs/freddie.jpeg' },
       ],
 
       chats: [
@@ -37,7 +38,7 @@ export default class App extends React.Component {
     };
   }
 
-  handleClickFooterButton = (page) => {
+  handleClickButtonFooter = (page) => {
     this.setState({ currentPage: page })
   }
 
@@ -45,15 +46,22 @@ export default class App extends React.Component {
     this.setState({ currentChat: chat, currentPage: 'Messages' })
   }
 
+  handleClickDeleteChat = (chatItem) => {
+    const chats = this.state.chats;
+    const filteredChats = chats.filter((chat) => chat !== chatItem);
+
+    this.setState({ chats: filteredChats })
+  }
+
   handleChangeChatInput = (e) => {
-    this.setState({ chatInput: e.target.value })
+    this.setState({ chatInput: e.target.value });
   }
 
   handleClickSendButton = (e) => {
     const currentChat = this.state.currentChat;
     const currentUser = this.state.currentUser;
-    const chatInput = this.state.chatInput;
     const messages = this.state.messages;
+    const chatInput = this.state.chatInput;
 
     const newMessage = {
       id: +new Date(),
@@ -61,15 +69,24 @@ export default class App extends React.Component {
       chatId: currentChat.id,
       time: new Date().toLocaleTimeString(),
       content: chatInput
-    };
+    }
 
     const newMessages = messages.concat(newMessage);
 
     this.setState({ messages: newMessages, chatInput: '' })
   }
 
+  handleClickDeleteMessage = (messageItem) => {
+    const messages = this.state.messages;
+    const currentChat = this.state.currentChat;
+
+    const filteredMessages = messages.filter((message) => message !== messageItem);
+
+    this.setState({ messages: filteredMessages })
+  }
+
   handleClickCreateNewChat = () => {
-    this.setState({ currentPage: 'Contacts' })
+    this.setState({ currentPage: 'Contacts' });
   }
 
   handleClickContact = (user) => {
@@ -81,33 +98,23 @@ export default class App extends React.Component {
     const isChatExist = chats.find((chat) => chat.name === user.name);
     if (isChatExist) return;
 
-
     const newChat = {
-      id: +new Date,
+      id: +new Date(),
       name: user.name,
       participants: [ currentUser, user.id ]
     }
 
     const newChats = chats.concat(newChat);
 
-    this.setState({ chats: newChats, currentPage: 'Messages', currentChat: newChat })
-
+    this.setState({ currentChat: newChat, currentPage: 'Messages', chats: newChats })
   }
 
-  handleClickDeleteChat = (chatItem) => {
-    const chats = this.state.chats;
-
-    const filteredChats = chats.filter((chat) => chat !== chatItem);
-
-    this.setState({ chats: filteredChats, currentChat: null })
+  handleClickLightTheme = () => {
+    this.setState({ theme: 'light' })
   }
 
-  handleClickDeleteMessage = (messageItem) => {
-    const messages = this.state.messages;
-
-    const filteredMessages = messages.filter((message) => message !== messageItem);
-
-    this.setState({ messages: filteredMessages })
+  handleClickDarkTheme = () => {
+    this.setState({ theme: 'dark' })
   }
 
   render() {
@@ -115,13 +122,14 @@ export default class App extends React.Component {
     const currentPage = this.state.currentPage;
     const currentChat = this.state.currentChat;
     const chatInput = this.state.chatInput;
+    const theme = this.state.theme;
 
     const users = this.state.users;
     const chats = this.state.chats;
     const messages = this.state.messages;
 
     return (
-      <div className="chat">
+      <div className={`chat ${theme}`}>
         <div className="header">
           <div className="pointer" style={{ width: '100px' }}>{(currentPage === 'Chats') ? 'Edit' : ''}</div>
           <div>{currentPage}</div>
@@ -134,6 +142,7 @@ export default class App extends React.Component {
               {users.map((user) => {
                 return (
                   <li onClick={() => this.handleClickContact(user)}>
+                    <img className="avatar" src={user.avatar} />
                     <span>{user.name}</span>
                     <span className={user.status}>{user.status}</span>
                   </li>
@@ -162,7 +171,29 @@ export default class App extends React.Component {
         {(currentPage === 'Settings') && (
           <div className="content settings">
             <span>{users.find((user) => user.id === currentUser).name}</span>
+            <div className="my-avatar">
+              <img src={users.find((user) => user.id === currentUser).avatar} />
+            </div>
             <span className="you"> (you)</span>
+            <div className="my-status">{users.find((user) => user.id === currentUser).status}</div>
+            <ul className="features">
+              <li>
+                <div>Themes</div>
+                <ul className="sub-menu">
+                  <li onClick={this.handleClickLightTheme}>Light</li>
+                  <li onClick={this.handleClickDarkTheme}>Dark</li>
+                </ul>
+              </li>
+              <li>
+                <div>Edit personal information</div>
+              </li>
+              <li>
+                <div>Language</div>
+              </li>
+              <li>
+                <div>Confidentiality</div>
+              </li>
+            </ul>
           </div>
         )}
 
@@ -178,7 +209,10 @@ export default class App extends React.Component {
                 return (
                   <li style={liStyle}>
                     <div>
-                      <div style={style}>{user.name} {message.time}</div>
+                      <div style={style}>
+                        <span>{user.name}</span>
+                        <span className="message-time"> {message.time}</span>
+                      </div>
                       <div style={style}>{message.content}</div>
                     </div>
                     <div className="delete" onClick={() => this.handleClickDeleteMessage(message)}>X</div>
@@ -186,18 +220,18 @@ export default class App extends React.Component {
                 )
               })}
             </ul>
-
             <div style={{ display: 'flex' }}>
-              <input style={{ flex: 1 }} type="text" placeholder="Type your message here" value={chatInput} onChange={this.handleChangeChatInput} />
+              <input style={{ flex: 1 }} placeholder="Type your message here" value={chatInput} onChange={this.handleChangeChatInput} />
               <button onClick={this.handleClickSendButton}>Send</button>
             </div>
+
           </div>
         )}
 
         <div className="footer">
-          <button onClick={() => this.handleClickFooterButton('Contacts')}>Contacts</button>
-          <button onClick={() => this.handleClickFooterButton('Chats')}>Chats</button>
-          <button onClick={() => this.handleClickFooterButton('Settings')}>Settings</button>
+          <button onClick={() => this.handleClickButtonFooter('Contacts')}>Contacts</button>
+          <button onClick={() => this.handleClickButtonFooter('Chats')}>Chats</button>
+          <button onClick={() => this.handleClickButtonFooter('Settings')}>Settings</button>
         </div>
       </div>
     );
